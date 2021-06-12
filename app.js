0a04e07f183e409fffb4627d6166b33fa881d3bc
abhaysinghs772 <abhaysinghs772@gmail.com>
2021-06-12 06:28:32 +0530

    modify gitignore add procfile upadte ports

--- app.js
+++ app.js
@@ -136,6 +136,13 @@ app.get("/about", function (req, res) {
     res.render("about");
 });
 
-app.listen(3000, function () {
-    console.log("server is started at port 3000");
+// setting up heroku's port
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8000;
+}
+app.listen(port);
+
+app.listen(port, function () {
+    console.log("server is started successfully");
 });
