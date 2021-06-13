a142323c24e8c363f4bc44b854e5a0b2ded9f6ca
abhaysinghs772 <abhaysinghs772@gmail.com>
2021-06-13 22:00:00 +0530

    minor bug fix

--- app.js
+++ app.js
@@ -139,9 +139,8 @@ app.get("/about", function (req, res) {
 // setting up heroku's port
 let port = process.env.PORT;
 if (port == null || port == "") {
-  port = 8000;
+  port = 3000;
 }
-app.listen(port);
 
 app.listen(port, function () {
     console.log("server is started successfully");
